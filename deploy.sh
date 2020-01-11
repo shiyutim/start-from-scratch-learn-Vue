026103d9ac04c5e9c7560c93d4c9d4d0e19deead
shiyutim <shiyutim@outlook.com>
2020-01-11 10:46:20 +0800

    'update'

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:shiyutim/start-from-scratch-learn-Vue.git master:gh-pages
 
 cd -
